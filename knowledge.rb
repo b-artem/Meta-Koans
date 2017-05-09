03596cb9d4261e0c60db91e9ba6002d9c92fafc4
b-artem <ba.artyom@gmail.com>
2017-05-09 23:38:27 +0300

    Minor changes

--- knowledge.rb
+++ knowledge.rb
@@ -1,20 +1,29 @@
-gem 'byebug'
+require 'byebug'
 
 puts 'Hello!'
 
 class Object
+
   def attribute(name, &block)
     puts name
 
     if name.instance_of?(Hash)
-      inst_var = ('@' + name.keys[0]).to_sym
+      inst_var = '@' + name.keys[0]
       method_name = name.keys[0]
       define_method(:initialize) do |*args|
         super(*args)
         instance_variable_set(inst_var, name.values[0])
       end
+    elsif block_given?
+      inst_var = '@' + name
+      method_name = name
+      define_method(:initialize) do |*args|
+        super(*args)
+        value = instance_eval(&block)
+        instance_variable_set(inst_var, value)
+      end
     else
-      inst_var = ('@' + name).to_sym
+      inst_var = '@' + name
       method_name = name
     end
 
@@ -22,9 +31,7 @@ class Object
     define_method((method_name + '=').to_sym) { |arg| instance_variable_set(inst_var, arg) }
     define_method((method_name + '?').to_sym) { instance_variable_get(inst_var) ? true : false }
 
-    instance_eval(&block) if block_given?
     # puts instance_methods.grep("/#{name}/")
-    # byebug
 
   end
 end
